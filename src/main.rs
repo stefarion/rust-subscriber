5b9b6d3f3b8c8ed620e8cacc449db55b3adac805
Stefanus Tan Jaya <stefanus.tan@ui.ac.id>
2025-05-13 16:13:53 +0700

    Simulation slow subscriber

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ impl MessageHandler<UserCreatedEventMessage> for UserCreatedHandler {
         let ten_millis = time::Duration::from_millis(1000);
         let now = time::Instant::now();
 
-        // thread::sleep(ten_millis);
+        thread::sleep(ten_millis);
 
         println!(
             "In Stefanus's Computer [2306152456]. Message received: {:?}",
